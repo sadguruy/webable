390d30926e28c5c19927032a9f1f98909b8d0cdb
Sadguru Y <sadguru003@gmail.com>
2026-04-28 16:14:21 +0530

    patch 0.0.7 updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,56 @@
 /// <reference types="vitest/config" />
-// vite.config.ts
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import react from "@vitejs/plugin-react";
+import { playwright } from "@vitest/browser-playwright";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-import { playwright } from '@vitest/browser-playwright';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+const dirname =
+	typeof __dirname !== "undefined"
+		? __dirname
+		: path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: "src/index.ts",
-      // all your exports here
-      formats: ["es"]
-    },
-    rollupOptions: {
-      external: ["react", "react/jsx-runtime"],
-      output: {
-        globals: {
-          react: "React"
-        }
-      }
-    }
-  },
-  test: {
-    projects: [{
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: playwright({}),
-          instances: [{
-            browser: 'chromium'
-          }]
-        }
-      }
-    }]
-  }
+	plugins: [react()],
+	build: {
+		lib: {
+			entry: "src/index.ts",
+			name: "WebableUI",
+			fileName: (format) => `index.${format}.js`,
+			formats: ["cjs", "es"],
+		},
+		rollupOptions: {
+			external: ["react", "react-dom", "react/jsx-runtime"],
+			output: {
+				globals: {
+					react: "React",
+				},
+			},
+		},
+	},
+	test: {
+		projects: [
+			{
+				extends: true,
+				plugins: [
+					storybookTest({
+						configDir: path.join(dirname, ".storybook"),
+					}),
+				],
+				test: {
+					name: "storybook",
+					browser: {
+						enabled: true,
+						headless: true,
+						provider: playwright({}),
+						instances: [
+							{
+								browser: "chromium",
+							},
+						],
+					},
+				},
+			},
+		],
+	},
 });
